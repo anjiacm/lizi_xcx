391cc4a4dfea4114d59f25dd2912e2b888336e06
wuguangan_mac <546167337@qq.com>
2017-12-14 19:34:50 +0800

    one
    第一次提交

---
+++ pages/index/index.js
@@ -0,0 +1,32 @@
+//index.js
+//获取应用实例
+const app = getApp()
+//获取搜索js
+const search = require('../../utils/search.js')
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    searchtext: '',
+    searchpage:{
+      pageshow:'',
+      searchtext:'',
+    },
+   
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //搜索事件处理
+  searchpagedo: function(event) {
+    var pageshow = event.currentTarget.dataset.pageindex;
+    var that = this;
+    that.setData({
+      searchpage: {
+        pageshow: pageshow,
+      },
+    });
+     
+  }
+  
+})
